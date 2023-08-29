bd297732f076e52ca91dcf185a5bbb3ee8629499
Jay-Ubisse <joaquimubisse@gmail.com>
2023-08-29 23:02:48 +0200

    first commit

---
+++ src/main/java/com/jayubisse/desktop/bank/system/DesktopBankSystem.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.jayubisse.desktop.bank.system;
+
+/**
+ *
+ * @author Axxel Milton
+ */
+public class DesktopBankSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
